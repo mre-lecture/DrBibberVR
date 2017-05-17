8d1f0a59ada401ddf6fa92838481de04e2ec911c
Hannah Büchner <1426783@stud.hs-mannheim.de>
2017-05-17 15:39:30 +0200

    added scripts to add highscore entry and to display highscore list

---
+++ Assets/Scripts/HighscoreEntry.cs
@@ -0,0 +1,35 @@
+﻿
+public class HighscoreEntry {
+
+	string username;
+	float time;
+
+	public HighscoreEntry(string u, float t)
+	{
+		username = u;
+		time = t;
+	}
+
+	public string getUsername()
+	{
+		return username;
+	}
+
+	public float getTime()
+	{
+		return time;
+	}
+
+	public float getTimeString()
+	{
+		return string.Format ("{0,5:0.00} sec.", time);
+	}
+
+	public bool newHighscore(float newTime)
+	{
+		if (time > newTime)
+			return true;
+		else
+			return false;
+	}
+}
---
+++ Assets/Scripts/HighscoreList.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class HighscoreList : MonoBehaviour
+{
+	List<HighscoreEntry> list;
+	public Text displayUsername, displayTime;
+	public InputField inputUsername;
+
+	void Start()
+	{
+		list = new List<HighscoreEntry> ();
+		addToList ();
+	}
+
+	void addToList()
+	{
+		// highscorelist can only have 10 entries -> should be dynamic
+		for(int i = 0; i < 10; i++)
+		{
+			if (PlayerPrefs.HasKey ("username" + i)) 
+			{
+				string user = PlayerPrefs.GetString ("username" + i);
+				string time = PlayerPrefs.GetFloat ("time" + i);
+				HighscoreEntry entry = new HighscoreEntry (user, time);
+				list.Add (entry);
+			}
+			else
+				break;
+		}
+
+		showList ();
+	}
+
+	void showList()
+	{
+		string username = "";
+		string t = "";
+
+		for (int i = 0; i < list.Count && i < 10; i++) 
+		{
+			username += list [i].getUsername () + "\n";
+			t += list [i].getTimeString () + "\n";
+		}
+
+		displayUsername.text = username;
+		displayTime.text = t;
+	}
+
+	public void addEntry()
+	{
+		// current: testEntry but read in typed username
+		float testTime = 0.00;
+		HighscoreEntry newEntry = HighscoreEntry("test", testTime);
+
+		bool added = false;
+		for (int i = 0; i < list.Count; i++) 
+		{
+			if (list [i].newHighscore (newEntry.getTime ())) 
+			{
+				list.Insert (i, newEntry);
+				added = true;
+				break;
+			}
+		}
+
+		if (!added)
+			list.Add (newEntry);
+
+		showList ();
+		saveList();
+	}
+
+	void saveList()
+	{
+		for (int i = 0; i < list.Count; i++) 
+		{
+			PlayerPrefs.SetString ("username" + i, list [i].getUsername ());
+			PlayerPrefs.SetFloat ("time" + i, list [i].getTime ());
+		}
+	}
+
+	public void deleteAll()
+	{
+		displayUsername = "";
+		displayTime = "";
+		list.Clear;
+		PlayerPrefs.DeleteAll ();
+	}
+
+	// maybe delete only one entry
+
+}
+
