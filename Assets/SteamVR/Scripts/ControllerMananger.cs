0085c9e29a4423bcbe3ee095b5e70f51989abfd2
Esraa Mustafa <isra.mirawdalli@gmail.com>
2017-05-23 00:16:14 +0200

    Deleted unnecessary files, structured, throwable objects

--- Assets/SteamVR/Scripts/ControllerMananger.cs
+++ Assets/SteamVR/Scripts/ControllerMananger.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControllerManager : MonoBehaviour {
+public class ControllerInput : MonoBehaviour {
 
 	public bool triggerButtonDown = false;
 
