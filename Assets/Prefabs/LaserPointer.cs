3cdcc4d815c57516948a3df97f9b978b6d071cba
Laura Most <Most-Laura@gmx.de>
2017-05-31 15:02:40 +0200

    Added script to start measuring time and added start button

---
+++ Assets/Prefabs/LaserPointer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointer : MonoBehaviour {
+	
+	// after Tutorial: https://www.raywenderlich.com/149239/htc-vive-tutorial-unity
+
+	private SteamVR_TrackedObject trackedObj;
+
+	// 1 - reference to Lasers Prefab
+	public GameObject laserPrefab;
+	// 2 - stores reference to instance of the laser
+	private GameObject laser;
+	// 3 - transform component is stored for ease of use
+	private Transform laserTransform;
+	// 4 - position where the laser hits
+	private Vector3 hitPoint; 
+
+	private SteamVR_Controller.Device Controller
+	{
+		get { return SteamVR_Controller.Input((int)trackedObj.index); }
+	}
+
+	void Awake()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+
+	private void ShowLaser(RaycastHit hit)
+	{
+		// 1 - show laser
+		laser.SetActive(true);
+		// 2 - position laser between controller and the point where the raycast hits
+		laserTransform.position = Vector3.Lerp(trackedObj.transform.position, hitPoint, .5f);
+		// 3 - point the laser at the position where the raycast hit
+		laserTransform.LookAt(hitPoint); 
+		// 4 - scale laser
+		laserTransform.localScale = new Vector3(laserTransform.localScale.x, laserTransform.localScale.y,
+			hit.distance);
+	}
+
+	// Use this for initialization
+	void Start () {
+		// 1 - spawn new laser and save reference to laser
+		laser = Instantiate(laserPrefab);
+		// 2 - store laser's transform
+		laserTransform = laser.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 1 - touchpad down
+		if (Controller.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+		{
+			RaycastHit hit;
+
+			// 2 - shoot ray from controller
+			if (Physics.Raycast(trackedObj.transform.position, transform.forward, out hit, 100))
+			{
+				hitPoint = hit.point;
+				ShowLaser(hit);
+			}
+		}
+		else // 3 - on touchpas release hide laser
+		{
+			laser.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Prefabs/StartGameOnCollision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameOnCollision : MonoBehaviour {
+
+	private float startTime;
+	private float elapsedTime;
+
+	void Awake(){
+		startTime = 0;
+	
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startTime != 0)
+		{
+			elapsedTime = Time.time - startTime;
+			Debug.Log (elapsedTime);
+
+		}
+	}
+
+	void OnTriggerEnter(){
+		startTime = Time.time;
+	}
+}
