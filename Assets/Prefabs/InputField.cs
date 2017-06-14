631bf33fe56ae0bf0ec7a614d215422d49000b00
Hannah Büchner <1426783@stud.hs-mannheim.de>
2017-06-14 12:52:50 +0200

    added keyboard and input collider

---
+++ Assets/Prefabs/InputField.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputField : MonoBehaviour
+{
+	public InputField ()
+	{
+	}
+
+	void OnTriggerEnter(){
+
+		SteamVR.instance.overlay.ShowKeyboard (0,0,"A",256,"",true,1);
+	}
+
+	void Awake(){
+	}
+	void Start(){
+	}
+	void Update(){
+	}
+
+}
+
