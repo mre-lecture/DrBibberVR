8b6d5bf62b9fd27143cf501d280c66d69d41ca34
Mafinex <public@vr.de>
2017-06-27 16:24:13 +0200

    lifes cna only be lost once a second now

--- Assets/LifeLost.cs
+++ Assets/LifeLost.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LifeLost : MonoBehaviour {
     public Healthbar life;
+    private string lastHit;
+    public Text timer;
 
 	// Use this for initialization
 	void Start () {
@@ -25,8 +28,12 @@ public class LifeLost : MonoBehaviour {
         if (other.gameObject.name.Equals("Hand1") || other.gameObject.name.Equals("Hand2") ) 
         {
             Debug.Log(other.gameObject.name + "TRIGGERED" );
-
-            life.BroadcastMessage("LifeLost");
+            if (!timer.text.Equals(lastHit))
+            {
+                lastHit = timer.text;
+                Debug.Log("lasthit + " + lastHit);
+                life.BroadcastMessage("LifeLost");
+            }
         }/* else if (other.transform  && other.transform.parent != null)
                Debug.Log(other.gameObject.name + " Parent TRIGGERED");
   
